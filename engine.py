5832ddcf4418c9efb35c0ad960c8f973877f9eab
ErebosLP <106330621+ErebosLP@users.noreply.github.com>
2022-07-25 11:24:05 +0200

    Add files via upload

--- engine.py
+++ engine.py
@@ -26,7 +26,6 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
             images_view1 = torch.unsqueeze(images[i][0].to(device),0)
             images_view2 = torch.unsqueeze(images[i][1].to(device),0)
             
-            
             # compute the embeddings
             z_view1 = model.forward(images_view1) # NxD
             z_view1 = torch.squeeze(z_view1,0)
@@ -43,23 +42,40 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
 
             # Compute the Region of Intrest
             sample_size = 64
-            pos = [np.random.normal(256/2,50-sample_size/2,1)[0],np.random.normal(256/2,200-sample_size/2,1)[0]]            
-            pos = np.round(pos).astype('uint8')
+            pos1 = [np.random.normal(256/2,50-sample_size/2,1)[0],np.random.normal(64 ,(100-sample_size/2)/3,1)[0]]            
+            pos1 = np.round(pos1).astype('uint8')
+            pos2 = [np.random.normal(256/2,50-sample_size/2,1)[0],np.random.normal(192,(100-sample_size/2)/3,1)[0]]            
+            pos2 = np.round(pos2).astype('uint8')
             
             # compute if valid RoI and correct it if not
-            if pos[0] < sample_size/2:
-                pos[0] = sample_size/2
-            if pos[0] > 256 - sample_size/2:
-                pos[0] = 256 - sample_size/2
+            if pos1[0] < sample_size/2:
+                pos1[0] = sample_size/2
+            if pos1[0] > 256 - sample_size/2:
+                pos1[0] = 256 - sample_size/2
                 
-            if pos[1] < sample_size/2:
-                pos[1] = sample_size/2
-            if pos[1] > 256 - sample_size/2:
-                pos[1] = 256 - sample_size/2
+            if pos1[1] < sample_size/2:
+                pos1[1] = sample_size/2
+            if pos1[1] > 128 - sample_size/2:
+                pos1[1] = 128 - sample_size/2
+                
+            if pos2[0] < sample_size/2:
+                pos2[0] = sample_size/2
+            if pos2[0] > 256 - sample_size/2:
+                pos2[0] = 256 - sample_size/2
+                
+            if pos2[1] < 128 + sample_size/2:
+                pos2[1] = 128 + sample_size/2
+            if pos2[1] > 256 - sample_size/2:
+                pos2[1] = 256 - sample_size/2
             
             # Cutt out the RoI of the embeddings
-            RoI_view1 = z_view1_norm[:,int(pos[0]-sample_size/2):int(pos[0]+sample_size/2),int(pos[1]-sample_size/2):int(pos[1]+sample_size/2) ]  
-            RoI_view2 = z_view2_norm[:,int(pos[0]-sample_size/2):int(pos[0]+sample_size/2),int(pos[1]-sample_size/2):int(pos[1]+sample_size/2) ]  
+            RoI1_view1 = z_view1_norm[:,int(pos1[0]-sample_size/2):int(pos1[0]+sample_size/2),int(pos1[1]-sample_size/2):int(pos1[1]+sample_size/2) ]  
+            RoI1_view2 = z_view2_norm[:,int(pos1[0]-sample_size/2):int(pos1[0]+sample_size/2),int(pos1[1]-sample_size/2):int(pos1[1]+sample_size/2) ]
+            
+            RoI2_view1 = z_view1_norm[:,int(pos2[0]-sample_size/2):int(pos2[0]+sample_size/2),int(pos2[1]-sample_size/2):int(pos2[1]+sample_size/2) ]  
+            RoI2_view2 = z_view2_norm[:,int(pos2[0]-sample_size/2):int(pos2[0]+sample_size/2),int(pos2[1]-sample_size/2):int(pos2[1]+sample_size/2) ]
+            
+            
             
             # #----------------------------------------------------------------------
             # test = images_view2.squeeze(0)[:,int(pos[0]-sample_size/2):int(pos[0]+sample_size/2),int(pos[1]-sample_size/2):int(pos[1]+sample_size/2) ]
@@ -84,19 +100,47 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
             # cross_weakly_norm = torch.div(cross_weakly,norm)
             
             
+            
+            
             #Compute Loss
-            RoI_view1_norm = (RoI_view1 - RoI_view1.mean(0).unsqueeze(0))/RoI_view1.std(0).unsqueeze(0)
-            RoI_view2_norm = (RoI_view2 - RoI_view2.mean(0).unsqueeze(0) )/RoI_view2.std(0).unsqueeze(0)
+
+            RoI1_view1_norm = torch.reshape((RoI1_view1 - RoI1_view1.mean(0).unsqueeze(0)) / RoI1_view1.std(0).unsqueeze(0),(sample_size**2,16)).unsqueeze(2)
+            RoI1_view2_norm = torch.reshape((RoI1_view2 - RoI1_view2.mean(0).unsqueeze(0)) / RoI1_view2.std(0).unsqueeze(0),(sample_size**2,16)).unsqueeze(2)
             
-            sim = torch.nn.CosineSimilarity(dim=0, eps=1e-08)(RoI_view1_norm,RoI_view2_norm)
-            sim = (sim + 1) / 2
+            RoI2_view1_norm = torch.reshape((RoI2_view1 - RoI2_view1.mean(0).unsqueeze(0)) / RoI2_view1.std(0).unsqueeze(0),(sample_size**2,16)).unsqueeze(2)
+            RoI2_view2_norm = torch.reshape((RoI2_view2 - RoI2_view2.mean(0).unsqueeze(0)) / RoI2_view2.std(0).unsqueeze(0),(sample_size**2,16)).unsqueeze(2)
+
+            # choose index for negative examples
+            neg_idx = np.random.choice(sample_size**2,(sample_size**2,5))
             
-            sim_diff = torch.abs(sim - 1)
-             
+            #negative examples
+            
+            neg1_view2_norm =  torch.swapaxes(RoI1_view2_norm[neg_idx,:].squeeze(),1,2)
+            neg2_view2_norm =  torch.swapaxes(RoI2_view2_norm[neg_idx,:].squeeze(),1,2)
+            
+            #Stack the vectorized embeddings
+            RoI1_view1_stack = torch.cat((RoI1_view1_norm,RoI1_view1_norm,RoI1_view1_norm,RoI1_view1_norm,RoI1_view1_norm,RoI1_view1_norm),dim=2)
+            neg1_view2_stack = torch.cat((RoI1_view2_norm,neg2_view2_norm),dim=2)
+            
+            RoI2_view1_stack = torch.cat((RoI2_view1_norm,RoI2_view1_norm,RoI2_view1_norm,RoI2_view1_norm,RoI2_view1_norm,RoI2_view1_norm),dim=2)
+            neg2_view2_stack = torch.cat((RoI2_view2_norm,neg1_view2_norm),dim=2)
+            
+            #Compute the similarities
+            sim1 = torch.nn.CosineSimilarity(dim=1, eps=1e-08)(RoI1_view1_stack,neg1_view2_stack)
+            sim1 = (sim1 + 1) / 2
+            sim1[:,0] = sim1[:,0] - 1
+            
+            sim2 = torch.nn.CosineSimilarity(dim=1, eps=1e-08)(RoI2_view1_stack,neg2_view2_stack)
+            sim2 = (sim2 + 1) / 2
+            sim2[:,0] = sim2[:,0] - 1
+            
+            
+            sim_diff = torch.abs(sim1).sum() + torch.abs(sim2).sum()
+            
             # # Substract the identity matrix and take the absolute value
             #cross_diff = torch.abs(cross_weakly_norm - torch.eye(sample_size*sample_size).cuda())
             # Sum up the loss over a batch
-            weakly_correlated_loss += sim_diff.sum()
+            weakly_correlated_loss += sim_diff
             
                 
              
